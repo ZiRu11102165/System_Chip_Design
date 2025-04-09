e8dad05ef087cc39d9b1561d3484ea963bce5040
ZiRu11102165 <124028666+ZiRu11102165@users.noreply.github.com>
2025-04-09 14:46:12 +0800

    Add files via upload

---
+++ code/PC/SnakeGame/SnakeGame/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace SnakeGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gfoodtext = new System.Windows.Forms.Label();
+            this.bfoodtext = new System.Windows.Forms.Label();
+            this.gfood = new System.Windows.Forms.Label();
+            this.bfood = new System.Windows.Forms.Label();
+            this.SerialPortSelect = new System.Windows.Forms.ComboBox();
+            this.SerialPortConnect = new System.Windows.Forms.Button();
+            this.SerialPortDisconnect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // gfoodtext
+            // 
+            this.gfoodtext.AutoSize = true;
+            this.gfoodtext.Font = new System.Drawing.Font("新細明體", 14F);
+            this.gfoodtext.Location = new System.Drawing.Point(41, 108);
+            this.gfoodtext.Name = "gfoodtext";
+            this.gfoodtext.Size = new System.Drawing.Size(129, 24);
+            this.gfoodtext.TabIndex = 1;
+            this.gfoodtext.Text = "Good Food : ";
+            // 
+            // bfoodtext
+            // 
+            this.bfoodtext.AutoSize = true;
+            this.bfoodtext.Font = new System.Drawing.Font("新細明體", 14F);
+            this.bfoodtext.Location = new System.Drawing.Point(56, 180);
+            this.bfoodtext.Name = "bfoodtext";
+            this.bfoodtext.Size = new System.Drawing.Size(110, 24);
+            this.bfoodtext.TabIndex = 2;
+            this.bfoodtext.Text = "Bad Food :";
+            // 
+            // gfood
+            // 
+            this.gfood.BackColor = System.Drawing.Color.Green;
+            this.gfood.Location = new System.Drawing.Point(176, 108);
+            this.gfood.Name = "gfood";
+            this.gfood.Size = new System.Drawing.Size(25, 25);
+            this.gfood.TabIndex = 3;
+            // 
+            // bfood
+            // 
+            this.bfood.BackColor = System.Drawing.Color.Red;
+            this.bfood.Location = new System.Drawing.Point(176, 186);
+            this.bfood.Name = "bfood";
+            this.bfood.Size = new System.Drawing.Size(25, 25);
+            this.bfood.TabIndex = 4;
+            // 
+            // SerialPortSelect
+            // 
+            this.SerialPortSelect.FormattingEnabled = true;
+            this.SerialPortSelect.Location = new System.Drawing.Point(895, 40);
+            this.SerialPortSelect.Name = "SerialPortSelect";
+            this.SerialPortSelect.Size = new System.Drawing.Size(152, 23);
+            this.SerialPortSelect.TabIndex = 5;
+            this.SerialPortSelect.SelectedIndexChanged += new System.EventHandler(this.SerialPortSelect_SelectedIndexChanged);
+            // 
+            // SerialPortConnect
+            // 
+            this.SerialPortConnect.Font = new System.Drawing.Font("新細明體", 14F);
+            this.SerialPortConnect.Location = new System.Drawing.Point(895, 82);
+            this.SerialPortConnect.Name = "SerialPortConnect";
+            this.SerialPortConnect.Size = new System.Drawing.Size(153, 66);
+            this.SerialPortConnect.TabIndex = 6;
+            this.SerialPortConnect.Text = "Connect";
+            this.SerialPortConnect.UseVisualStyleBackColor = true;
+            this.SerialPortConnect.Click += new System.EventHandler(this.SerialPortConnect_Click);
+            // 
+            // SerialPortDisconnect
+            // 
+            this.SerialPortDisconnect.Font = new System.Drawing.Font("新細明體", 14F);
+            this.SerialPortDisconnect.Location = new System.Drawing.Point(895, 161);
+            this.SerialPortDisconnect.Name = "SerialPortDisconnect";
+            this.SerialPortDisconnect.Size = new System.Drawing.Size(153, 72);
+            this.SerialPortDisconnect.TabIndex = 7;
+            this.SerialPortDisconnect.Text = "Disconnect";
+            this.SerialPortDisconnect.UseVisualStyleBackColor = true;
+            this.SerialPortDisconnect.Click += new System.EventHandler(this.SerialPortDisconnect_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1082, 553);
+            this.Controls.Add(this.SerialPortDisconnect);
+            this.Controls.Add(this.SerialPortConnect);
+            this.Controls.Add(this.SerialPortSelect);
+            this.Controls.Add(this.bfood);
+            this.Controls.Add(this.gfood);
+            this.Controls.Add(this.bfoodtext);
+            this.Controls.Add(this.gfoodtext);
+            this.Name = "Form1";
+            this.Text = "Snake Game";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label gfoodtext;
+        private System.Windows.Forms.Label bfoodtext;
+        private System.Windows.Forms.Label gfood;
+        private System.Windows.Forms.Label bfood;
+        private System.Windows.Forms.ComboBox SerialPortSelect;
+        private System.Windows.Forms.Button SerialPortConnect;
+        private System.Windows.Forms.Button SerialPortDisconnect;
+    }
+}
+
---
+++ code/PC/SnakeGame/SnakeGame/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace SnakeGame
+{
+    public partial class Form1 : Form
+    {
+        const int ROW = 16, COL = 16;
+        const int GRID_WIDTH = 25, GRID_HEIGHT = 25;
+        const int GAME_POS_X = 200, GAME_POS_Y = 20;
+
+        public Task ReceiveSerial;
+
+        StringBuilder port = new StringBuilder();
+        SerialPort SerialPort = new SerialPort();
+
+        Color WIN = Color.Yellow;
+        Color CONTINUE = Color.Yellow;
+        Color LOSE = Color.Green;
+        Color BG_COLOR = Color.Black;
+
+        Color BORDER = Color.Gray;
+        Color GFOOD = Color.Green;
+        Color BFOOD = Color.Red;
+        Color EMPTY = Color.Black;
+
+        Color[,] GridsColor = new Color[ROW, COL];
+        Label[,] grids = new Label[ROW, COL];
+
+        
+        int score = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            SerialPortSelect.Items.AddRange(SerialPort.GetPortNames());
+
+            for (int i = 0; i < ROW; i++)
+            {
+                for (int j = 0; j < COL; j++)
+                {
+                    grids[i, j] = new Label();
+                    grids[i, j].Width = GRID_WIDTH;
+                    grids[i, j].Height = GRID_HEIGHT;
+                    
+                    grids[i, j].BackColor = BG_COLOR;
+                    grids[i, j].Left = j * GRID_WIDTH + GAME_POS_X;
+                    grids[i, j].Top = i * GRID_HEIGHT + GAME_POS_Y;
+                    grids[i, j].Visible = true;
+
+                    this.Controls.Add(grids[i, j]);
+                }
+            }
+
+            SerialPortDisconnect.Enabled = false;
+            
+        }
+
+        private void SerialPortSelect_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            port.Append(SerialPortSelect.Text);
+        }
+
+        private void SerialPortConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SerialPort.PortName = port.ToString();
+                SerialPort.BaudRate = 115200;
+                SerialPort.Parity = Parity.None;
+                SerialPort.DataBits = 8;
+                SerialPort.StopBits = StopBits.One;
+                SerialPort.Open();
+                SerialPortConnect.Enabled = false;
+                SerialPortDisconnect.Enabled = true;
+
+                if (SerialPort.IsOpen)
+                {
+                    Receive_Data();
+                }
+            }
+            catch(Exception exex)
+            {
+                MessageBox.Show(exex.Message);
+                SerialPort.Close();
+
+            }
+        }
+
+        private void SerialPortDisconnect_Click(object sender, EventArgs e)
+        {
+            SerialPortSelect.Enabled = true;
+            SerialPortConnect.Enabled = false;
+            SerialPort.Close();
+        }
+
+        private void Receive_Data()
+        {
+            try { 
+                ReceiveSerial = new Task(Receive_Data_avilable);
+
+                ReceiveSerial.Start();
+            }
+            catch(Exception exec)
+            {
+                MessageBox.Show(exec.Message);
+            }
+        }
+
+        private void Receive_Data_avilable()
+        {
+            int row = 0, col = 0 ,i = 0;
+            //int number = 256;
+            int number = 256;
+            byte[] ReceiveData = new byte[number];
+
+            while (SerialPort.IsOpen)
+            {
+                SerialPort.DiscardInBuffer();
+                //MessageBox.Show("check serialportselect again");
+                //Task.Delay(1).Wait();
+                while (SerialPort.BytesToRead != (256));
+                    Task.Delay(5);
+
+                SerialPort.Read(ReceiveData, 0, SerialPort.BytesToRead);
+
+                for (i = 0; i < number; i++)
+                {
+                    
+                    switch (ReceiveData[i])
+                    {
+                        case 0:
+                            GridsColor[row, col] = EMPTY;
+                            break;
+                        case 1:
+                            GridsColor[row, col] = BORDER;
+                            break;
+                        case 2:
+                            GridsColor[row, col] = CONTINUE;
+                            break;
+                        case 3:
+                            GridsColor[row, col] = GFOOD;
+                            break;
+                        case 4:
+                            GridsColor[row, col] = BFOOD;
+                            break;
+                        default:
+                            break;
+                    }
+                    Console.Write($"{ReceiveData[i]}");
+                    col = col + 1;
+                    if (col == COL)
+                    {
+                        row = row + 1;
+                        col = 0;
+                        Console.WriteLine();
+                    }
+                    if (row == ROW)
+                    {
+                        row = 0;
+                        col = 0;
+
+                    }
+                }
+                //Console.WriteLine($"{ Convert.ToString(ReceiveData[256],16)}");
+                //SerialPort.DiscardInBuffer();
+                UpadteScreen();
+                
+                //SerialPort.DiscardInBuffer();
+            }
+        }
+        private void UpadteScreen()
+        {
+            //if(grids.InvokeRequired)
+            this.Invoke(new EventHandler(delegate
+            {
+                for (int i = 0; i < ROW; i++)
+                {
+                    for (int j = 0; j < COL; j++)
+                    {
+                        grids[i, j].BackColor = GridsColor[i, j];
+                    }
+                }
+            }));
+        }
+    }
+}
